17a0a51e3dcb6e386f3de19f4ffa05adedfa9622
ikanmakarel01 <moskovkokop@gmail.com>
2025-03-19 07:34:02 +0700

    Update segitiga.py

--- segitiga.py
+++ segitiga.py
@@ -23,8 +23,25 @@
 #     print()
 #     i = i - 1
 
+
+# for i in range(a, 0, -1):
+#     for j in range(0, i):
+#         print("*", end="")
+#     print()
+
+
 a = 5
-for i in range(a, 0, -1):
-    for j in range(0, i):
-        print("*", end="")
-    print()
+for i in range(1, a + 1):  # Loop dari 1 ke a (meningkat)
+    for j in range(a - i):  # Cetak spasi dulu
+        print(" ", end="")
+    for j in range(i):  # Cetak simbol #
+        print("#", end="")
+    print("")  # Pindah ke baris baru
+
+a = 5
+for i in range(a, 0, -1):  # Loop menurun dari 5 ke 1
+    for j in range(a - i):  # Cetak spasi untuk meratakan ke kanan
+        print(" ", end="")
+    for j in range(i):  # Cetak simbol #
+        print("#", end="")
+    print("")  # Pindah ke baris baru
